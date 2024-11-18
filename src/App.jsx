f955d4b8a546be4802887b91f6c4fdf53cd9e439
Nathan Paul <mail@polarhive.net>
2024-11-18 18:52:33 +0530

    feat: analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from './components/Navbar';
 import Hero from './pages/Hero';
 import Cart from './pages/Cart';
@@ -25,8 +26,8 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/logout" element={<LogoutPage />} />
           <Route path="/signup" element={<SignupPage />} />
-
         </Routes>
+        <Analytics />
       </div>
     </Router>
   );
